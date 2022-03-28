e43e3b5288d42916901428ebe11c0fee2ec332ae
joshchunZ <87585587+joshchunZ@users.noreply.github.com>
2022-03-28 16:04:45 -0400

    update

--- testing.sh
+++ testing.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 mkdir -p testing_script
 
-TEST="testing_script/in_script_text.txt"
-OUT="testing_script/out.txt"
+TEST="./testing_script/in_script_text.txt"
+OUT="./testing_script/out.txt"
 MYPROG=''
 TESTPROG=''
 
 echo -e "Downlading python program ... \n"
 wget https://raw.githubusercontent.com/joshhchun/Passcode-Breaker-Testscript/main/test.py -q -O ./testing_script/test.py > /dev/null
+chmod +x ./testing_script/test.py
+
+
 
 
 for i in $(seq 15); do
     ./testing_script/test.py > $OUT 
-    cat out.txt | tail -n 9 > $TEST
-    TESTPROG=$(cat out.txt | head -n 1)
+    cat $OUT | tail -n 9 > $TEST
+    TESTPROG=$(cat $OUT | head -n 1)
     MYPROG=$(./exe/PC07 $TEST)
 
     echo "$MYPROG"; echo "$TESTPROG"
@@ -25,5 +28,4 @@ for i in $(seq 15); do
     NUM=$NUM-1
 done
 
-rm -r testing_script
 
