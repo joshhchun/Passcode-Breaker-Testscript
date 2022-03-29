5394d3c8f1864aaa449c55926c1dbc9f5df3613f
joshchunZ <87585587+joshchunZ@users.noreply.github.com>
2022-03-29 14:50:08 -0400

    update

--- testing.sh
+++ testing.sh
@@ -5,6 +5,7 @@ TEST="./testing_script/in_script_text.txt"
 OUT="./testing_script/out.txt"
 MYPROG=''
 TESTPROG=''
+SCORE=0
 
 echo -e "Downlading python program ... \n"
 wget https://raw.githubusercontent.com/joshhchun/Passcode-Breaker-Testscript/main/test.py -q -O ./testing_script/test.py > /dev/null
@@ -20,11 +21,12 @@ for i in $(seq 15); do
      echo "Correct cracked password: $TESTPROG"
     if [[ "$MYPROG" == "$TESTPROG" ]]; then
         echo -e "CORRECT!\n"
+        SCORE=$(($SCORE+1))
     else 
         echo -e "oops incorrect...\n" 
     fi
 done
 
+echo "Total Score: $SCORE / 15 = $(($SCORE / 15 * 100))%"
+
 rm -r testing_script
-
-
